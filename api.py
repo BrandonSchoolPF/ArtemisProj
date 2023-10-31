ad24b92e406ec2c3c7e35d5d3ab12c89608a4558
Brandon Ly <brandonly@Brandons-MBP.home>
2023-10-31 11:12:53 -0400

    Changed API.py to include implementation to flask

--- api.py
+++ api.py
@@ -2,10 +2,10 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 #API Info
-#access_token = "X-RapidAPI-Key": "INSERT KEY"
+#access_token = "X-RapidAPI-Key" : "25649f62f2msh3808137a2442080p186039jsn6c56c32c82f1"
 API_Add = 'https://adsbexchange-com1.p.rapidapi.com/v2/lat/27.943721/lon/-82.537932/dist/5/'
 #API_Host = "X-RapidAPI-Host": "adsbexchange-com1.p.rapidapi.com"
-headers = {	"X-RapidAPI-Key": "INSERT KEY",	"X-RapidAPI-Host": "adsbexchange-com1.p.rapidapi.com"}
+headers = {	"X-RapidAPI-Key" : "25649f62f2msh3808137a2442080p186039jsn6c56c32c82f1",	"X-RapidAPI-Host" : "adsbexchange-com1.p.rapidapi.com"}
 
 
 # Make the request to the API
--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 import json
-
-API = 'https://adsbexchange-com1.p.rapidapi.com/v2/lat/27.943721/lon/-82.537932/dist/5/'
+from api import *
+from flask_compress import Compress
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    req = requests.get(API)
-    data = req.content
-    print(req.content)
-    return render_template('index.html', data= data)
 
+
+@app.route("/")
+@app.route('/index')
+def index():
+    API_Add = 'https://adsbexchange-com1.p.rapidapi.com/v2/lat/27.943721/lon/-82.537932/dist/5/'
+    headers = {	"X-RapidAPI-Key" : "25649f62f2msh3808137a2442080p186039jsn6c56c32c82f1",	"X-RapidAPI-Host" : "adsbexchange-com1.p.rapidapi.com"}
+    response = requests.get(API_Add, headers=headers)
+    
+    if response.status_code != 200: 
+        print("Call did not work")
+    else: 
+        data = response.json()
+        return jsonify(data)
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
