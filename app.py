5277339a454453bba4cbe0fcbb8a4f2ec943d989
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2023-11-01 12:06:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     API_Add = 'https://adsbexchange-com1.p.rapidapi.com/v2/lat/27.943721/lon/-82.537932/dist/5/'
-    headers = {	"X-RapidAPI-Key" : "25649f62f2msh3808137a2442080p186039jsn6c56c32c82f1",	"X-RapidAPI-Host" : "adsbexchange-com1.p.rapidapi.com"}
+    headers = {	"X-RapidAPI-Key" : "INSERT KEY",	"X-RapidAPI-Host" : "adsbexchange-com1.p.rapidapi.com"}
     response = requests.get(API_Add, headers=headers)
     
     if response.status_code != 200: 
